b4f40a63505c58b79748f347d13ede906eceebb6
bagusromadhonwn <64753911+bagusromadhonwn@users.noreply.github.com>
2020-09-13 02:29:41 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,6 +1,6 @@
 // prettier-ignore
 export default [
-    { author: "Bagus_ro", quotes: "Happybirth day" },
+
     { author: "Bagus_Ro", quotes: "Sorry telat ngawene" },
     { author: "bagus_ro", quotes: "Hey! I wuv chuuuu! UwU" },
     { author: "bagus_ro", quotes: "I may not with you everyday, but I love you everyday." },
