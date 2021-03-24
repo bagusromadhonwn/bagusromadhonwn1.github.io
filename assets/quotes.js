e7fb1374a17dcf732a1ee2cd61792a3c249b87a8
bagusromadhonwn <64753911+bagusromadhonwn@users.noreply.github.com>
2021-03-25 00:34:57 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,10 +2,7 @@
 export default [
 
     { author: "Bagusrown", quotes: "Semangat Kuliah Amengg🤗" },
-    { author: "bagusrown", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "bagusrown", quotes: "I may not with you everyday, but I hope u always happy with him" },
-    { author: "bagusrown", quotes: "Yeah,even though I know you won't be able to write sweet stories about our relationship. I'm sorry, but 831." },
-    { author: "bagusrown", quotes: "Hehehe UwUw kan." },
+    { author: "bagusrown", quotes: "Uwes yaa!!☺." },
     
     
 ];
