e3e240294fdf9cbdc8d19f64203f26ea6fe578d3
bagusromadhonwn <64753911+bagusromadhonwn@users.noreply.github.com>
2021-11-21 19:11:24 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,7 +1,7 @@
 // prettier-ignore
 export default [
 
-    { author: "Bagusrown", quotes: "Selamat ulang tahun reginat" },
+    { author: "Bagusrown", quotes: "Selamat ulang tahun reginataaaaaaaaaaa" },
     { author: "bagusrown", quotes: "Semoga panjang umur dan sehat selalu, ojo judes teros mukamu:v." },
     
     
