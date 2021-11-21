4bd0c87ecfc2e96cd369dafe0810b1e5c9c5b6fe
bagusromadhonwn <64753911+bagusromadhonwn@users.noreply.github.com>
2021-11-21 19:20:46 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,7 +2,6 @@
 export default [
 
     { author: "Bagusrown", quotes: "Selamat ulang tahun reginataaaaaaaaaaa" },
-    { author: "bagusrown", quotes: "Semoga panjang umur dan sehat selalu, ojo judes teros mukamu:v, koyok reog nko malah." },
     
     
 ];
