c2195ac9375f3fa50844dcbb41c7095934ebf50c
bagusromadhonwn <64753911+bagusromadhonwn@users.noreply.github.com>
2022-01-26 10:41:38 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,7 +1,7 @@
 // prettier-ignore
 export default [
 
-    { author: "Bagus.rown", quotes: "Selamat ulang tahun reginataaaaaaaaaaa" },
+    { author: "Bagus.rown", quotes: "i always proud of you" },
     
     
 ];
