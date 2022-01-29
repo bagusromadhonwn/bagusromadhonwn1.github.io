714af25180ee8b17e470be9f1c9af2d741360a36
bagusromadhonwn <64753911+bagusromadhonwn@users.noreply.github.com>
2022-01-29 09:11:25 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import config from "./config";
 const { host, port } = config;
 
 export default {
-  ssr: "true",
+  Mode: "spa",
   /*
    ** Router base for github pages deployment.
    ** Need Documentation
